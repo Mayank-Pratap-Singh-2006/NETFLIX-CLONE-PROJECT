b78c5bf11d2acc3f01ba1272ef17bde130d7af02
Mayank Pratap Singh <mayankpratap1004@gmail.com>
2026-01-06 15:14:27 +0530

    Modified CSS

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-// Select elements
-const emailInput = document.querySelector(".email-box input");
-const button = document.querySelector(".email-box button");
-const message = document.getElementById("message");
+fullname="Mayank Pratap Singh"
+console.log(fullname);
+console.history(fullname);
 
-// Button click event
-button.addEventListener("click", () => {
-  const email = emailInput.value.trim();
-
-  // Reset previous state
-  message.className = "";
-  button.classList.remove("active-btn");
-
-  // Empty email
-  if (email === "") {
-    message.textContent = "Email is required";
-    message.classList.add("error");
-    return;
-  }
-
-  // Invalid email
-  if (!email.includes("@")) {
-    message.textContent = "Enter a valid email address";
-    message.classList.add("error");
-    return;
-  }
-
-  // Success
-  message.textContent = "Email accepted. Welcome!";
-  message.classList.add("success");
-  button.classList.add("active-btn");
-});
